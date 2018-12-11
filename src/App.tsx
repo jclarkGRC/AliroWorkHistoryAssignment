7c5fad02d236869952bef1b48e7e217885310054
Joshua Clark <joshua.clark@tee2.org>
2018-12-11 15:42:21 -0800

    fixed missing components redux store and mui theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import * as React from "react";
 import { Component } from "react";
 import { HashRouter } from 'react-router-dom';
 import routes from './routes';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import {blue} from "@material-ui/core/colors";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+const store = createStore(rootReducer);
+const theme = createMuiTheme({
+    palette:{
+        primary: blue,
+    }
+})
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HashRouter>
-          {routes}
-        </HashRouter>
+        <MuiThemeProvider theme={theme}>
+          <Provider store={store}>
+            <HashRouter>
+              {routes}
+            </HashRouter>
+          </Provider>
+        </MuiThemeProvider>
       </div>
     );
   }
