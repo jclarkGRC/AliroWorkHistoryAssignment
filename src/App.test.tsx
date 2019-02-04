87670e0b937863dcb58100ee2bf61e6b06b971df
Joseph Bethards <joseph.bethards@t2.local>
2019-02-04 13:24:39 -0800

    Added document headers

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,35 @@
+/*
+* App.test.tsx
+* App testing the pwa-starter-application-npm-ts
+*
+* Created by Josh Clark 12/11/18
+*
+* pwa-starter-application-npm-ts
+*
+* Copyright © 2009-2019 United States Government as represented by
+* the Chief Information Officer of the DHA Connected Health. All Rights Reserved.
+*
+* Copyright © 2009-2019 Contributors. All Rights Reserved.
+*
+* THIS OPEN SOURCE AGREEMENT ("AGREEMENT") DEFINES THE RIGHTS OF USE,
+* REPRODUCTION, DISTRIBUTION, MODIFICATION AND REDISTRIBUTION OF CERTAIN
+* COMPUTER SOFTWARE ORIGINALLY RELEASED BY THE UNITED STATES GOVERNMENT
+* AS REPRESENTED BY THE GOVERNMENT AGENCY LISTED BELOW ("GOVERNMENT AGENCY").
+* THE UNITED STATES GOVERNMENT, AS REPRESENTED BY GOVERNMENT AGENCY, IS AN
+* INTENDED THIRD-PARTY BENEFICIARY OF ALL SUBSEQUENT DISTRIBUTIONS OR
+* REDISTRIBUTIONS OF THE SUBJECT SOFTWARE. ANYONE WHO USES, REPRODUCES,
+* DISTRIBUTES, MODIFIES OR REDISTRIBUTES THE SUBJECT SOFTWARE, AS DEFINED
+* HEREIN, OR ANY PART THEREOF, IS, BY THAT ACTION, ACCEPTING IN FULL THE
+* RESPONSIBILITIES AND OBLIGATIONS CONTAINED IN THIS AGREEMENT.
+*
+* Government Agency: DHA Connected Health
+* Government Agency Original Software Designation: pwa-starter-application-npm-ts
+* Government Agency Original Software Title: pwa-starter-application-npm-ts
+* User Registration Requested. Please send email
+* with your contact information to: robert.a.kayl.civ@mail.mil
+* Government Agency Point of Contact for Original Software: robert.a.kayl.civ@mail.mil
+*
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
