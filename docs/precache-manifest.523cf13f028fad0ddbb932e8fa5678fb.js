4a2b8b960b925ba085d31c5f4e249ca36cfbd2c8
zeebot <jjclark@attain.com>
2019-07-20 17:06:27 -0700

    updated docs folder

---
+++ docs/precache-manifest.523cf13f028fad0ddbb932e8fa5678fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a889ad4659b9834783e5e3ba2a54e566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "023ae26c370d9a910fa2",
+    "url": "./static/css/main.84ba1065.chunk.css"
+  },
+  {
+    "revision": "c59af32893454ceb6aeb",
+    "url": "./static/js/2.5e371ed6.chunk.js"
+  },
+  {
+    "revision": "023ae26c370d9a910fa2",
+    "url": "./static/js/main.87f2f03b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
